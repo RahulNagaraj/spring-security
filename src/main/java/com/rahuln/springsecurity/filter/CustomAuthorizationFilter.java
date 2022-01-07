9e0542793ab4f09c563c430d8e8f88af890ec265
Rahul N <rahul.nagarajmv@gmail.com>
2022-01-07 15:03:53 -0600

    refresh token

--- src/main/java/com/rahuln/springsecurity/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/rahuln/springsecurity/filter/CustomAuthorizationFilter.java
@@ -30,12 +30,12 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (request.getServletPath().equals("/api/login")) {
+        if (request.getServletPath().equals("/api/login")
+                || request.getServletPath().equals("/api/token/refresh")) {
             filterChain.doFilter(request, response);
         } else {
 
             String authorizationHeader = request.getHeader(AUTHORIZATION);
-            log.info("authorization header {}", authorizationHeader);
             if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
                 try {
                     String token = authorizationHeader.substring("Bearer ".length());
